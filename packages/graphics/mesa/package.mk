8731d457d4c38db8f1cf571fb1736167add64a0c
PakoSt <kokotekilata@gmail.com>
2021-01-30 23:15:58 +0000

    Update mesa flags

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -18,23 +18,24 @@ PKG_MESON_OPTS_TARGET="-Ddri-drivers=${DRI_DRIVERS// /,} \
                        -Dgallium-drivers=${GALLIUM_DRIVERS// /,} \
                        -Dgallium-extra-hud=false \
                        -Dgallium-xvmc=false \
-                       -Dgallium-omx=disabled \
+                       -Dgallium-omx=auto \
                        -Dgallium-nine=false \
-                       -Dgallium-opencl=disabled \
                        -Dvulkan-drivers= \
-                       -Dshader-cache=true \
+                       -Dshader-cache=enabled \
                        -Dshared-glapi=true \
                        -Dopengl=true \
-                       -Dgbm=true \
-                       -Degl=true \
+                       -Dgbm=enabled \
+                       -Degl=enabled \
                        -Dglvnd=false \
                        -Dasm=true \
                        -Dvalgrind=false \
                        -Dlibunwind=false \
+                       -Dllvm=true \
                        -Dlmsensors=false \
                        -Dbuild-tests=false \
                        -Dselinux=false \
-                       -Dosmesa=none"
+                       -Dglx=dri \
+                       -Dshared-glapi=enabled"
 
 if [ "$TARGET_ARCH" = "i386" ]; then
   PKG_MESON_OPTS_TARGET="${PKG_MESON_OPTS_TARGET//-Dvulkan-drivers=auto/-Dvulkan-drivers=}"
@@ -43,48 +44,48 @@ fi
 if [ "$DISPLAYSERVER" = "x11" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET xorgproto libXext libXdamage libXfixes libXxf86vm libxcb libX11 libxshmfence libXrandr libglvnd"
   export X11_INCLUDES=
-  PKG_MESON_OPTS_TARGET+=" -Dplatforms=x11 -Ddri3=true -Dglx=dri"
+  PKG_MESON_OPTS_TARGET+=" -Dplatforms=x11 -Ddri3=enabled -Dglx=dri"
 elif [ "$DISPLAYSERVER" = "weston" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET wayland wayland-protocols"
-  PKG_MESON_OPTS_TARGET+=" -Dplatforms=wayland -Ddri3=false -Dglx=disabled"
+  PKG_MESON_OPTS_TARGET+=" -Dplatforms=wayland -Ddri3=disabled -Dglx=disabled"
 elif [ "$DISTRO" = "Lakka" ]; then
   PKG_DEPENDS_TARGET+=" glproto dri2proto dri3proto presentproto xorgproto libXext libXdamage libXfixes libXxf86vm libxcb libX11 libxshmfence xrandr systemd openssl"
   export X11_INCLUDES=
-  PKG_MESON_OPTS_TARGET+=" -Dplatforms=x11 -Ddri3=true -Dglx=dri"
+  PKG_MESON_OPTS_TARGET+=" -Dplatforms=x11 -Ddri3=enabled -Dglx=dri -Degl=enabled -Dlibunwind=disabled -Dvalgrind=disabled -Dllvm=enabled -Dgallium-xvmc=disabled"
 else
-  PKG_MESON_OPTS_TARGET+=" -Dplatforms= -Ddri3=false -Dglx=disabled"
+  PKG_MESON_OPTS_TARGET+=" -Dplatforms= -Ddri3=false -Dglx=false"
 fi
 
 if [ "$LLVM_SUPPORT" = "yes" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET elfutils llvm"
   export LLVM_CONFIG="$SYSROOT_PREFIX/usr/bin/llvm-config-host"
-  PKG_MESON_OPTS_TARGET+=" -Dllvm=true"
+  PKG_MESON_OPTS_TARGET+=" -Dllvm=enabled"
 else
-  PKG_MESON_OPTS_TARGET+=" -Dllvm=false"
+  PKG_MESON_OPTS_TARGET+=" -Dllvm=disabled"
 fi
 
 if [ "$VDPAU_SUPPORT" = "yes" -a "$DISPLAYSERVER" = "x11" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libvdpau"
-  PKG_MESON_OPTS_TARGET+=" -Dgallium-vdpau=true"
+  PKG_MESON_OPTS_TARGET+=" -Dgallium-vdpau=enabled"
 else
-  PKG_MESON_OPTS_TARGET+=" -Dgallium-vdpau=false"
+  PKG_MESON_OPTS_TARGET+=" -Dgallium-vdpau=disabled"
 fi
 
 if [ "$VAAPI_SUPPORT" = "yes" ] && listcontains "$GRAPHIC_DRIVERS" "(r600|radeonsi)"; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libva"
-  PKG_MESON_OPTS_TARGET+=" -Dgallium-va=true"
+  PKG_MESON_OPTS_TARGET+=" -Dgallium-va=enabled"
 else
-  PKG_MESON_OPTS_TARGET+=" -Dgallium-va=false"
+  PKG_MESON_OPTS_TARGET+=" -Dgallium-va=disabled"
 fi
 
 if listcontains "$GRAPHIC_DRIVERS" "vmware"; then
-  PKG_MESON_OPTS_TARGET+=" -Dgallium-xa=true"
+  PKG_MESON_OPTS_TARGET+=" -Dgallium-xa=enabled"
 else
-  PKG_MESON_OPTS_TARGET+=" -Dgallium-xa=false"
+  PKG_MESON_OPTS_TARGET+=" -Dgallium-xa=disabled"
 fi
 
 if [ "$OPENGLES_SUPPORT" = "yes" ]; then
-  PKG_MESON_OPTS_TARGET+=" -Dgles1=false -Dgles2=true"
+  PKG_MESON_OPTS_TARGET+=" -Dgles1=disabled -Dgles2=enabled"
 else
-  PKG_MESON_OPTS_TARGET+=" -Dgles1=false -Dgles2=false"
+  PKG_MESON_OPTS_TARGET+=" -Dgles1=disabled -Dgles2=enabled"
 fi
