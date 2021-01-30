1d471f509849269e94504c96e65cada2fc3760ea
PakoSt <kokotekilata@gmail.com>
2021-01-30 23:16:07 +0000

    Update atf

--- packages/tools/atf/package.mk
+++ packages/tools/atf/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Team LibreELEC
 
 PKG_NAME="atf"
-PKG_VERSION="v2.1"
-PKG_SHA256="7c4c00a4f28d3cfbb235fd1a1fb28c4d2fc1d657c9301686e7d8824ef575d059"
+PKG_VERSION="f928897fa5df6afa0f70bb1b1011e4bb03cb4596"
+PKG_SHA256="5161a07ed9065c876cd0d37bd9b544b77a707bcde9d0cdf86b83c7b0b5bfa95b"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="BSD-3c"
 PKG_SITE="https://github.com/ARM-software/arm-trusted-firmware"
-PKG_URL="https://github.com/ARM-software/arm-trusted-firmware/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/crust-firmware/arm-trusted-firmware/archive/$PKG_VERSION.tar.gz"
 PKG_LONGDESC="ARM Trusted Firmware is a reference implementation of secure world software, including a Secure Monitor executing at Exception Level 3 and various Arm interface standards."
 PKG_TOOLCHAIN="manual"
 PKG_IS_KERNEL_PKG="yes"
