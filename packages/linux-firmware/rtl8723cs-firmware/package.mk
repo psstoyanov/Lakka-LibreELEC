6d52247712fe62eb3ee5a310cc478c03c4707cd7
PakoSt <kokotekilata@gmail.com>
2021-02-03 18:02:08 +0000

    Attempt to create a package for rtl8723cs-firmware

---
+++ packages/linux-firmware/rtl8723cs-firmware/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="rtl8723cs-firmware"
+PKG_VERSION="2371a976e3c4de6bdfd505b45b1b3682943e1302"
+PKG_SHA256="c5aeb0325f74ac24c0ef5a720fd2c391ef664c66561e9397edd7c624c08b7674"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/Icenowy/rtl8723cs"
+PKG_URL="https://github.com/Icenowy/rtl8723cs/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Linux driver for Realtek RTL8723CS chip"
+
+
