d953d94cd9606915ee5781b5d72d31929474387b
PakoSt <kokotekilata@gmail.com>
2021-02-22 19:05:11 +0000

    Bump python/devel packages

--- packages/python/devel/Mako/package.mk
+++ packages/python/devel/Mako/package.mk
@@ -3,15 +3,15 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="Mako"
-PKG_VERSION="1.1.1"
-PKG_SHA256="2984a6733e1d472796ceef37ad48c26f4a984bb18119bb2dbc37a44d8f6e75a4"
+PKG_VERSION="1.1.4"
+PKG_SHA256="17831f0b7087c313c0ffae2bcbbd3c1d5ba9eeac9c38f2eb7b50e8c99fe9d5ab"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pypi.org/project/Mako"
-PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="Python3:host setuptools:host MarkupSafe:host"
 PKG_LONGDESC="Mako is a super-fast templating language that borrows the best ideas from the existing templating languages."
 PKG_TOOLCHAIN="manual"
 
 makeinstall_host() {
-  exec_thread_safe python3 setup.py install --prefix=$TOOLCHAIN
+  exec_thread_safe python3 setup.py install --prefix=${TOOLCHAIN}
 }
